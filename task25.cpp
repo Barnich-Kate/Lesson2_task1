9919609c35d71efae9d0b936b0569088a4956444
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-30 21:05:22 +0300

    Create task25.cpp
    Add the solution of the task

---
+++ task25.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	float x, f;
+	cout << x;
+	if ((x > 2)||(x<-2)) {
+		f = 2 * x;
+	}else {
+		f = -3* x;
+	}
+	cout << "f(x) = " << f << endl;
+	return 0;
+}
+    
